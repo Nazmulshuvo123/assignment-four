bfba6adff889f5132988c3f8c55ca7da9dcad82d
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-11 14:59:33 +0600

    Calculate Admission Final Score

---
+++ problem-four.js
@@ -0,0 +1,34 @@
+function calculateFinalScore(obj) {
+    if(typeof obj !== 'object'){
+        return "Invalid Input";
+    }
+
+    const { name, testScore, schoolGrade, isFFamily } = obj;
+
+    obj.isFFamily = 20;
+
+    let totalScore = obj.testScore + obj.schoolGrade + obj.isFFamily;
+    if(totalScore < 80){
+        return false;
+    }
+    
+
+    if(typeof name !== 'string' || typeof testScore !== 'number' || typeof schoolGrade !== 'number' || isFFamily === true){
+
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+
+console.log(calculateFinalScore({ name: "Rajib", testScore: 45,  schoolGrade: 25, isFFamily : true  }));
+console.log(calculateFinalScore({ name: "Rajib", testScore: 45,  schoolGrade: 25, isFFamily : false }));
+console.log(calculateFinalScore("hello"));
+console.log(calculateFinalScore({ name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true }));
+
+
+
+
